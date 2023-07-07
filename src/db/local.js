34eab2c96e9c80c20add431d5160568c8015f8c2
Pablo Henrique SC <pabloph0077@gmail.com>
2023-07-07 12:51:58 -0300

    refactor: banco de dados local

---
+++ src/db/local.js
@@ -0,0 +1,26 @@
+let DB = {
+  games: [
+    {
+      id: 11,
+      title: "Call of duty MW",
+      year: 2019,
+      price: 60,
+    },
+
+    {
+      id: 22,
+      title: "Sea of Thieves",
+      year: 2018,
+      price: 40,
+    },
+
+    {
+      id: 33,
+      title: "Minecraft",
+      year: 2012,
+      price: 20,
+    },
+  ],
+};
+
+module.exports = DB;
